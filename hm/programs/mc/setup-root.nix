7129d3544816d9e5cd574146c468af6a91354c14
ramblehead <v.rybynok@gmail.com>
2024-07-16 22:48:20 +0100

    Checkpoint

---
+++ hm/programs/mc/setup-root.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  lib,
+}: {
+  home.activation = {
+    setupMcWrapper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      BARC=/etc/bash.bashrc
+      if ! grep 'alias.*mc=' $BARC \
+         &>/dev/null
+      then
+        # if [ "$(tail -c 1 $BARC | xxd -p)" != "0a" ]; then
+        # if [ "$(tail -c 1 $BARC | wc -l)" -ne 1 ]; then
+        if (( "$(tail -c 1 $BARC | wc -l)" < 1 )); then
+          echo >> $BARC
+        fi
+
+        echo -e "\nalias mc='. ${pkgs.mc}/libexec/mc/mc-wrapper.sh'" >> $BARC
+      fi
+    '';
+  };
+}
--- system/etc/profile.d/hm-session-vars.sh
+++
@@ -1,3 +0,0 @@
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/hm-session-vars.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/hm-session-vars.sh'
-fi
