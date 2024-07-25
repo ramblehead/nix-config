c4be764e0868608c7862f2416eef4c9d524744fb
Victor Rybynok <victor.rybynok@toshiba.eu>
2024-07-25 17:07:38 +0100

    Checkpoint

--- hm/programs/nix/setup-debian.nix
+++ hm/programs/nix/setup-debian.nix
@@ -4,7 +4,8 @@
   lib,
 }: {
   setupNix = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ln -fs ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh \
+    run ln -fs  $VERBOSE_ARG \
+      ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh \
       /etc/profile.d
   '';
 }
--- hm/programs/sudo/setup-debian.nix
+++ hm/programs/sudo/setup-debian.nix
@@ -3,13 +3,14 @@
   lib,
 }: {
   setupSudoers = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    SUDS=/etc/sudoers
+    readonly SUDS=/etc/sudoers
     if ! grep '^.*secure_path=.*/nix/var/nix/profiles/default/s\?bin.*$' \
-       $SUDS &>/dev/null
+       run $SUDS &>/dev/null
     then
       readonly BIN=/nix/var/nix/profiles/default/bin
       readonly SBIN=/nix/var/nix/profiles/default/sbin
-      sed -E 's|^(.*secure_path="/usr/local/sbin:/usr/local/bin)(.*")$|\1:'$SBIN:$BIN'\2|' -i $SUDS
+      run sed -E 's|^(.*secure_path="/usr/local/sbin:/usr/local/bin)(.*")$|\
+      \1:'$SBIN:$BIN'\2|' -i $SUDS
     fi
   '';
 }
