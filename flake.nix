aa20f60829511e433824b99a36896abbaf28f22f
ramblehead <v.rybynok@gmail.com>
2025-01-02 11:14:54 +0000

    Checkpoint

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
--- hm/hosts/qt-dl1.nix
+++ hm/hosts/qt-dl1.nix
@@ -5,24 +5,11 @@
   lib,
   flakeRoot,
   ...
-}: let
-  inherit (inputs.nixgl.packages."${pkgs.system}") nixGLIntel;
-in {
-  imports = [
-    # todo: remove when https://github.com/nix-community/home-manager/pull/5355 gets merged:
-    # see https://github.com/Smona/home-manager/blob/nixgl-compat/modules/misc/nixgl.nix
-    #     https://github.com/giggio/dotfiles/blob/main/home-manager/home.nix
-    #     https://github.com/giggio/dotfiles/blob/main/home-manager/flake.nix
-    #     https://github.com/nix-community/home-manager/issues/3968
-    #     https://github.com/nix-community/home-manager/pull/5355
-    # Provides nixGLwrap function
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "0g5yk54766vrmxz26l3j9qnkjifjis3z2izgpsfnczhw243dmxz9";
-    })
-  ];
-
-  nixGL.prefix = "${nixGLIntel}/bin/nixGLIntel";
+}: {
+  nixGL.packages = inputs.nixgl.packages;
+  nixGL.defaultWrapper = "mesa";
+  #nixGL.offloadWrapper = "nvidiaPrime";
+  nixGL.installScripts = ["mesa"];
 
   home.packages = let
     utils-cli = (import (flakeRoot + /software/selections/utils-cli.nix)) {
@@ -44,11 +31,6 @@ in {
     ++ utils-gui.packages
     ++ database.packages
     ++ (with pkgs; [
-      # nixgl.nixGLIntel
-      # nixgl.auto.nixGLDefault
-      # nixgl.nixVulkanIntel
-      nixGLIntel
-
       # (config.lib.nixGL.wrap wayland)
       # (config.lib.nixGL.wrap gnome.gdm)
       # (config.lib.nixGL.wrap gnome.gnome-shell)
