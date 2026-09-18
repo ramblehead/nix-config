58173aec7b6ba7858b53178705390cc190cc58b4
ramblehead <v.rybynok@gmail.com>
2026-09-18 03:07:47 +0100

    Checkpoint

--- hm/users/rh-vostok.nix
+++ hm/users/rh-vostok.nix
@@ -24,8 +24,8 @@ in {
     ANTHROPIC_BASE_URL = "https://api.deepseek.com/anthropic";
     ANTHROPIC_MODEL = "deepseek-v4-pro[1m]";
     ANTHROPIC_DEFAULT_OPUS_MODEL = "deepseek-v4-pro[1m]";
-    ANTHROPIC_DEFAULT_SONNET_MODEL = "deepseek-v4-pro[1m]";
-    ANTHROPIC_DEFAULT_HAIKU_MODEL = "deepseek-v4-flash";
+    ANTHROPIC_DEFAULT_SONNET_MODEL = "deepseek-v4-flash[1m]";
+    ANTHROPIC_DEFAULT_HAIKU_MODEL = "deepseek-flash";
     CLAUDE_CODE_SUBAGENT_MODEL = "deepseek-v4-pro[1m]";
 
     CLAUDE_CODE_AUTO_COMPACT_WINDOW = "786432";
